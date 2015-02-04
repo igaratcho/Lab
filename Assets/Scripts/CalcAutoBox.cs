a0ade9bb8f218109d30b76975ae3827d685614d4
YoheiIgarashi <igaratcho@gmail.com>
2015-02-05 02:05:03 +0900

    ボックスコライダーの自動生成テスト

---
+++ Assets/Scripts/CalcAutoBox.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CalcAutoBox : MonoBehaviour 
+{
+	[SerializeField]Vector3 start;
+	[SerializeField]Vector3 end;
+
+	void Start()
+	{
+		DoTest ();
+	}
+
+	void DoTest()
+	{
+		float height = 3.0f;
+
+		Vector3 center = Vector3.Lerp(start, end, 0.5f);
+		center.y = height * 0.5f;
+
+		BoxCollider box = this.gameObject.AddComponent<BoxCollider> ();
+		box.center = center;
+		box.size = new Vector3(end.x-start.x, height, end.z-start.z);
+	}
+
+	void OnDrawGizmos()
+	{
+		BoxCollider box = this.gameObject.GetComponent<BoxCollider> ();
+		if(box) {
+			Gizmos.DrawCube (box.center, box.size);
+		}
+	}
+
+}
